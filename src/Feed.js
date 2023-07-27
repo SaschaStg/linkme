b4d8bf1d40c05204677ea9c1426293d654186a40
Schmidt Sascha <ss532@hdm-stuttgart.de>
2023-07-27 16:33:22 +0200

    add firebase and feed post function

--- src/Feed.js
+++ src/Feed.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Feed.css"
 import CreateIcon from '@mui/icons-material/Create';
 import InputOption from './InputOption';
@@ -7,16 +7,45 @@ import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
 import EventNoteIcon from '@mui/icons-material/EventNote';
 import CalendarViewDayIcon from '@mui/icons-material/CalendarViewDay';
 import Post from './Post';
+import { db } from './firebase'
+import { firebase } from './firebase';
 
 function Feed() {
+    const [input, setInput] = useState("")
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        db.collection("posts").onSnapshot((snapshot) =>
+            setPosts(snapshot.docs.map(doc => (
+                {
+                    id: doc.id,
+                    data: doc.data(),
+                }
+            ))));
+    })
+
+    const sendPost = (event) => {
+        event.preventDefault()
+        db.collection("posts").add({
+            name: "Sascha Schmidt",
+            description: "Test message",
+            message: input,
+            photoUrl: "",
+            timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+        })
+
+        setInput("")
+
+    }
+
     return (
         <div className="feed">
             <div className="feed__inputContainer">
                 <div className="feed__input">
                     <CreateIcon />
                     <form>
-                        <input type="text" />
-                        <button type="submit">Send</button>
+                        <input value={input} onChange={e => setInput(e.target.value)} type="text" />
+                        <button onClick={sendPost} type="submit">Send</button>
                     </form>
                 </div>
                 <div className="feed__inputOptions">
@@ -27,7 +56,9 @@ function Feed() {
                 </div>
             </div>
             {/* Posts */}
-            <Post name="Sascha Schmidt" description="Test message" message="Hello World" />
+            {posts.map(({ id, data: { name, description, message, photoUrl } }) => (
+                <Post key={id} name={name} description={description} message={message} photoUrl={photoUrl} />
+            ))}
         </div>
     )
 }
